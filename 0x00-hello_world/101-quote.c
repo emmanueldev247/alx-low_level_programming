8edcd9a6455968ccf642c279cbdf95acf2ad5169
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-15 17:05:31 -0700

    not simple without printf & puts/putchar :|

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - \
-				Dora Korpar,2015-10-19";
-	fwrite(message, sizeof(char), sizeof(message) - 1, stderr);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
