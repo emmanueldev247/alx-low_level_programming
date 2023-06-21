db3bf038dc5a5199dd8fef27519fc9b7d11a35e6
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-20 17:20:58 -0700

    lowercase -main()

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description - prints strings without standard functions
+ * print_alphabet - loop to do description
+ * Description - prints alphabet without standard functions
  * Return: Returns 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
