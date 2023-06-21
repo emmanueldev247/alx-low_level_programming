69c2bd661ce2c4a77d9f00fd4ce9a83d34ec3330
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-20 19:00:03 -0700

    sigh :(

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _islower - function that prints the sign of a number
+ * @n:  Parameter to check
+ * Return: Returns 1 or 0 or -1
+ */
+int print_sign(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		i = 1;
+	} else if (n == 0)
+	{
+		_putchar('0');
+		i = 0;
+	} else
+	{
+		_putchar('-');
+		i = -1;
+	}
+	return (i);
+}
