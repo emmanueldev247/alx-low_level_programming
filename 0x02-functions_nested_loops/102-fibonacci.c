e18d8070242324c1c27cef0be8b67c08dd4445a0
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-21 16:08:07 -0700

    fibonacci - first 50 +fixes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - funtion to print first 100 Fibonacci numbers
- * Returns: returns 0 (Success)
+ * main - function to print first 100 Fibonacci numbers
+ * Return: returns 0 (Success)
  */
 int main(void)
 {
 	long int a, b, c, i, sum;
+
 	a = 1;
 	b = 2;
-	
 	sum = a + b;
 	printf("%ld, %ld, %ld", a, b, sum);
-	for (i = 0; i < 48; i++)
+	for (i = 0; i < 47; i++)
 	{
 		c = b;
 		b = sum;
