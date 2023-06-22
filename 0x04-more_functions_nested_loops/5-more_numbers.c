94b2736ec9c4518bcbf43945d71532f4c09c315b
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-22 08:28:48 -0700

    numbers 0-14 x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - function to print numbers 0 - 14 x10
+ */
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
