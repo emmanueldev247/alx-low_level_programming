b3506bb4bd7639757a19b7928f75fccdedf88dd8
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-22 09:59:54 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		} else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
