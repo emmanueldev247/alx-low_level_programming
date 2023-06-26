e3da5ad4cfc45dc543940011f9889e75e512f979
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-26 16:29:42 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - function updates its argument to 98
+ * @n: pointer to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+**A directory for 0x05 pointers, arrays & strings project**
