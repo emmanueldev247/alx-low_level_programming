411ea8010eff8f635d6dd5ccadf3dba21fdba093
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-26 22:00:26 -0700

    halved string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: string to be printed that way
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
