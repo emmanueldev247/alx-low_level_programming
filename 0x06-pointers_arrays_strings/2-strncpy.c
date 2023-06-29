dd6a5c193f83b6c18b4b64e290f625e3a2abc596
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-29 10:59:26 -0700

    strncpy function clone

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - function to copy a string at limit  n
+ * @dest: destination string
+ * @src: source string
+ * @n: number of character from src to be copied to dest
+ * Return: return a pointer to the resulting string in dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int src_len, i;
+
+	src_len = 0;
+
+	while (src[src_len] != '\0')
+		src_len++;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
+		if (i > src_len++)
+			dest[i] = '\n';
+	}
+
+	return (dest);
+}
