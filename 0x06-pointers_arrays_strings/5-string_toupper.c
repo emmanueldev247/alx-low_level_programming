39fcc87cb022c33c5dc8a69a7048fb2567d83cca
Emmanuel Ademola <mailemmydee@gmail.com>
2023-06-30 16:40:07 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * string_toupper - function to change all lowercase to uppercase
+ * Return: returns string after conversion
+ */
+char *string_toupper(char *text)
+{
+	int i, len;
+
+	char *changed;
+
+	changed = text;
+
+	len = 0;
+
+	while (changed[len] != '\0')
+	{
+		len++;
+	}
+	
+	for (i = 0; i < len; i++)
+	{
+		if ((changed[i] > 96) && (changed[i] < 123))
+		{
+			changed[i] = changed[i] - 32;
+		}
+	}
+	return (changed);
+}
