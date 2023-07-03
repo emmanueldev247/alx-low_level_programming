5f022741738dc1bdcd61a1d165bf09dbf91d6f69
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-03 15:54:22 -0700

    infinite add (half done)

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * Return: pointer to the result of the addition
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	unsigned int *num1, *num2;
+
+
+
+
+
+
+
+
+	return (r);
+}
