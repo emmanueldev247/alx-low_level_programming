8963eb7455ce07e418db80af2930ad0452f3ea0a
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-06 14:41:03 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,41 +6,43 @@
  */
 int _sqrt_recursion(int n)
 {
-	int start;
-	int stop;
-	int mid;
 	int result;
 
 	if ((n == 0) || (n == 1))
 		return (n);
 
-	start = 1;
-	stop = n;
+	result =  sqrt_helper(n, 1, n);
 
-	while (start <= n)
-	{
-		mid = start + (stop - start) / 2;
-		result = mid;
-		if (mid == (n / mid))
-		{
-			result = mid;
-			break;
-		}
-		if (mid < (n / mid))
-		{
-			start = mid + 1;
-			result = mid;
-		}
-		else
-		{
-			stop = mid - 1;
-			result = mid;
-		}
-	}
+	if (result == -1)
+		return (-1);
 
 	if (result * result == n)
 		return (result);
-	else
-		return (-1);
+
+	return (-1);
 }
 
+/**
+ * sqrt_helper - helper function to find the natural square root of a number
+ * @n: number to be square-rooted
+ * @start: start number for binary search
+ * @stop: stop number for binary search
+ * Return: the square root of n or -1
+ */
+int sqrt_helper(int n, int start, int stop)
+{
+	int mid;
+
+	if (start > stop)
+		return (-1);
+
+	mid = start + (stop - start) / 2;
+
+	if (mid * mid == n)
+		return (mid);
+
+	if (mid < (n / mid))
+		return (sqrt_helper(n, mid + 1, stop));
+	else
+		return (sqrt_helper(n, start, mid - 1));
+}
