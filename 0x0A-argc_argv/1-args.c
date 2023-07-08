7daa7b74744d0b9718c1c4a055cbb1a59b0d8c5c
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-07 17:41:20 -0700

    number of args passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
