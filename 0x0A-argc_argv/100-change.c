e2c3a94eaac827b7b2da171e8571bab607ba36cb
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-07 19:29:11 -0700

    get those change +fixes2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
@@ -11,23 +10,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int val, count, j;
+	int val, count;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
-	}
-
-	j = 0;
-	while (argv[1][j] != '\0')
-	{
-		if (!isdigit(argv[1][j]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		j++;
 	}
 
 	val = atoi(argv[1]);
@@ -38,50 +26,21 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	count = count_helper(val);
-
-	printf("%d\n", count);
-	return (0);
-}
-
-/**
- * count_helper - function to count change
- * @val: amount that requires change
- * Return: the amount of change
- */
-int count_helper(int val)
-{
-	int count;
-
 	count = 0;
 	while (val != 0)
 	{
 		if (val >= 25)
-		{
 			val -= 25;
-			count++;
-		}
 		else if (val >= 10)
-		{
 			val -= 10;
-			count++;
-		}
 		else if (val >= 5)
-		{
 			val -= 5;
-			count++;
-		}
 		else if (val >= 2)
-		{
 			val -= 2;
-			count++;
-		}
 		else
-		{
 			val -= 1;
-			count++;
-		}
+		count++;
 	}
-
-	return (count);
+	printf("%d\n", count);
+	return (0);
 }
