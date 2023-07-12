d183793ec70cd3b55cca23c76618b8ac905029aa
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-12 12:06:54 -0700

    concatenating strings (redo)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,20 +1,19 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * str_concat - function that concatenates two strings
+ * str_concat - function to concatenate two strings
  * @s1: first string
- * @s2: second srting
+ * @s2: second string
  *
- * Return: a pointer tot he concatenated string
+ * Return: pointer to concatenated string
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *con_str;
-	unsigned int len1, len2, i;
+	unsigned int len1, len2, i, j;
+	char *biggie;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
@@ -26,18 +25,19 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2] != '\0')
 		len2++;
 
-	con_str = malloc(sizeof(char) * (len1  + len2 + 1));
+	biggie = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (con_str == NULL)
+	if (biggie == NULL)
 	{
-		free(con_str);
+		free(biggie);
 		return (NULL);
 	}
-
+	
 	for (i = 0; i < len1; i++)
-		con_str[i] = s1[i];
+		biggie[i] = s1[i];
 
-	for (i = i; i < (len1 + len2); i++)
-		con_str[i] = s2[i - len1];
-	return (con_str);
+	for (j = i; j < (len1 + len2); j++)
+		biggie[j] = s2[j - len1];
+	biggie[j]  = '\0';
+	return (biggie);
 }
