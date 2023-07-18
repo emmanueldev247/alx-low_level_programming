b0888611dab553b581ccf7c0dc94c1f3808b4cdb
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-18 14:44:27 -0700

    print dog +fixes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,18 @@
  */
 void print_dog(struct dog *d)
 {
-	unsigned int i;
-
 	if (d != NULL)
 	{
-		for (i = 0; i < sizeof(*d) / sizeof(d[0]); i++)
-		{
-			if (d->name == NULL)
-				d->name = "(nil)";
-
-			if (d->owner == NULL)
-				d->owner = "(nil)";
-
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
 			printf("Name: %s\n", d->name);
-			printf("Age: %.1f\n", d->age);
+
+		printf("Age: %.1f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
 			printf("Owner: %s\n", d->owner);
-		}
-	}
+	}	
 }
