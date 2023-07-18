798b47e46b10949803d438abeee1629f011af736
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-18 13:34:22 -0700

    poppy struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a type with elements
+  * @name: name
+  * @age: age
+  * @owner: owner
+  *
+  */
+typedef struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
