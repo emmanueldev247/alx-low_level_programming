324aed97ff81c59549c15c7a0165acaaf691823b
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-19 18:57:02 -0700

    fixes to task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,24 +15,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
-	if (d != NULL)
+
+	if (d == NULL || name == NULL || owner == NULL)
 	{
-		d->name = strdup(name);
-		if (d->name == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
+		free(d);
+		return (NULL);
+	}
 
-		d->age = age;
+	d->name = strdup(name);
+	if (d->name == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 
-		d->owner = strdup(owner);
-		if (d->owner == NULL)
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
+	d->age = age;
+
+	d->owner = strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
 	}
 	return (d);
 }
