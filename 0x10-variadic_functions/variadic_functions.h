eb0c83cc0f4600b670dd6920a0db910679925bd4
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-27 06:14:57 -0700

    header updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,25 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(char c);
+/**
+ * struct func - struct for specifiers and function pair
+ * @specifier: specifier
+ * @function: function
+ */
+typedef struct func
+{
+	char *specifier;
+	void (*function)(va_list);
+} func_arg;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void printint(va_list args);
+void printchr(va_list args);
+void printstr(va_list args);
+void printfloat(va_list args);
 
 #endif
