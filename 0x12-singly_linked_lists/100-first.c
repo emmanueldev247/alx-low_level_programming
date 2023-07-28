043c846eddc6fc67095be16c3ffc9c9ed91681aa
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-27 17:11:49 -0700

    ahead ahead

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_message - function to print text before main is executed
+ *
+ * Return: nothing
+ */
+
+void print_message()
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
