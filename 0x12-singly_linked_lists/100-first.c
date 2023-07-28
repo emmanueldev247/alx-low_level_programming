395c645531d36c68e3587be57bfdd0bac21743bd
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-27 17:12:36 -0700

    ahead ahead

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,8 @@
  * Return: nothing
  */
 
-void print_message()
+void print_message(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
