41f2ff5f5dc26d349b841805daa31259f37f5ade
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-31 14:30:41 -0700

    Free singly linked list v2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a linked list
+ * @head: head of the singly list passed
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	current = (*head)->next;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
