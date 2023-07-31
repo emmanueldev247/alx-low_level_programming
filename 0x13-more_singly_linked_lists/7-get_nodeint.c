37f242c9649b1481e75ab65aa0cf74ab8573fc50
Emmanuel Ademola <mailemmydee@gmail.com>
2023-07-31 16:44:07 -0700

    Get node at index +fixes 3

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,9 +15,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i <= index; i++)
 	{
-		if (head == NULL)
-			return (NULL);
-		head = head->next;
+		if (head != NULL)
+			head = head->next;
 	}
 
 
