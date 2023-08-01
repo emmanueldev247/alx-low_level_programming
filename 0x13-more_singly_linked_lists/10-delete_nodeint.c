65834a1facaf1e5cfe7bae88c27eaf2240621751
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-01 15:20:52 -0700

    delete node +fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (i = 0; i < index - 1 && current; i++)
 		current = current->next;
 
-	if ((current == NULL || current->next == NULL) && index > 0)
+	if (current == NULL || current->next == NULL)
 		return (-1);
 
 	temp = current->next;
