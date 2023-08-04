c0ffe505230d76f10c8866bc126bbb14cf5c9b80
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-03 18:55:13 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+/**
+ * get_endianness - function to check the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int num;
+	char *ptr;
+
+	num = 1;
+	ptr = (char *)&num;
+
+	if (*ptr == 1)
+	{
+		return (1);
+	}
+    else
+		return (0);
+}
+
