f2cef8f028b69cb5f0f5d3991ec40c281989be29
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-06 08:25:10 -0700

    Infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,6 +3,8 @@
  * infinite_add - function that adds two numbers
  * @n1: first number
  * @n2: second number
+ * @r: buffer to store the result
+ * @size_r: buffer size
  * Return: pointer to the result of the addition
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -12,11 +14,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	num1 = atoi(n1);
 	num2 = atoi(n2);
-	sum = num1 +num2;
+	sum = num1 + num2;
 	sprintf(r, "%d", sum);
 
 	len = 0;
-	while(r[len] != '\0')
+	while (r[len] != '\0')
 		len++;
 	if (size_r < len)
 		return (0);
