f75a8313e5620676762bb7c8325849614773cc7e
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-06 11:47:46 -0700

    multiplies 2x + fixes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -121,8 +121,6 @@ int main(int argc, char *argv[])
 			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	printf("%s\n", nout);
-
-	free(nout);
+	printf("%s\n", nout), free(nout);
 	return (0);
 }
