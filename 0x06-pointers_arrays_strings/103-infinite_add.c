494f32b4f22d115ea9f0ff133cd6ce0c9eae3b1f
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-06 08:21:41 -0700

    Infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,14 +7,19 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	unsigned int *num1, *num2;
+	unsigned int num1, num2, sum;
+	int len;
 
+	num1 = atoi(n1);
+	num2 = atoi(n2);
+	sum = num1 +num2;
+	sprintf(r, "%d", sum);
 
-
-
-
-
-
+	len = 0;
+	while(r[len] != '\0')
+		len++;
+	if (size_r < len)
+		return (0);
 
 	return (r);
 }
