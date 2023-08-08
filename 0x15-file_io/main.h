6456b55ff4c80430e89571cb286848dfaa028dfa
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-07 17:46:16 -0700

    Header updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
