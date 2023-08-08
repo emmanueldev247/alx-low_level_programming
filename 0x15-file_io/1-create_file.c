4722037192d01ff5214a60a767bf2c2c5560b2ef
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-07 18:44:47 -0700

    Create file +fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,22 +14,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	len = 0;
-	while (text_content[len] != '\0')
-		len++;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
-		write(fd, "", 0);
-	else
-	{
-		bytesWritten = write(fd, text_content, len);
-		if (bytesWritten == -1)
-			return (-1);
-	}
+		text_content = "";
+
+	len = 0;
+	while (text_content[len] != '\0')
+		len++;
+
+	bytesWritten = write(fd, text_content, len);
+	if (bytesWritten == -1)
+		return (-1);
 
 	close(fd);
 
