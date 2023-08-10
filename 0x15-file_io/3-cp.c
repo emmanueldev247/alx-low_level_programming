60f7c363fc32d949fb61a531223f99fb75a9bca7
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-10 01:25:05 -0700

    fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,85 +1,71 @@
 #include "main.h"
+
 /**
- * display_error_and_exit - Display an error message and exit
- * @msg: The error message format.
- * @filename: The filename or related information.
- * @code: The exit code to be used.
- *
- * Return: nothing
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void display_error_and_exit(const char *msg, const char *filename, int code)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	dprintf(STDERR_FILENO, msg, filename);
-	exit(code);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
- * copy_files - Copy contents from one file to another.
- * @file_from: Source file name.
- * @file_to: Destination file name.
- *
- * Return: nothing
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-void copy_files(const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	char buffer[1024];
-	ssize_t byte_read, byte_written;
-
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		display_error_and_exit("Error: Can't read from file %s\n", file_from, 98);
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		display_error_and_exit("Error: Can't write to %s\n", file_to, 99);
-	}
-
-	while ((byte_read = read(fd_from, buffer, 1024)) > 0)
-	{
-		byte_written = write(fd_to, buffer, byte_read);
-		if (byte_written != byte_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			display_error_and_exit("Error: Can't write to %s\n", file_from, 99);
-		}
-	}
-
-	if (byte_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		display_error_and_exit("Error: Can't read from file %s\n", file_from, 98);
-	}
-
-	close(fd_from);
-	close(fd_to);
-}
-
-/**
- * main - Entry point of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- *
- * Return: 0 on success, other values on failure.
- */
-int main(int argc, char **argv)
-{
-	const char *file_from, *file_to;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	file_from = argv[1];
-	file_to = argv[2];
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	copy_files(file_from, file_to);
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
