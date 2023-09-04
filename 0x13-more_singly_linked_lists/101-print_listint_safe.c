0b70a788fabbb10e1ea2595019d3b518d8321093
Emmanuel Ademola <mailemmydee@gmail.com>
2023-09-04 14:23:45 -0700

    print list safe (mod)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,44 +1,68 @@
 #include "lists.h"
 /**
- * print_listint_safe - function that prints all the elements of a linked list
- * @head: the singly list passed
+ * free_listp - frees a linked list
+ * @head: head of a list.
  *
- * Return: the number of nodes
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int i;
-	const listint_t *slow, *fast;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
 
-	i = 0;
-	slow = head;
-	fast = head;
-
-	while (fast && fast->next)
+	hptr = NULL;
+	while (head != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		new = malloc(sizeof(listp_t));
 
-		if (slow == fast)
-			break;
+		if (new == NULL)
+			exit(98);
 
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		i++;
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
 
-		if (fast && fast->next)
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			fast = fast->next;
-			i++;
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
 		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
 	}
 
-	while (slow && slow != fast)
-	{
-
-		printf("[%p] %d\n", (void *)slow, slow->n);
-			slow = slow->next;
-			i++;
-	}
-
-	return (i);
+	free_listp(&hptr);
+	return (nnodes);
 }
