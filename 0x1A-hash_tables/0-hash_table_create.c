d275d3e6ba9d92e3a9a813f6222b735cd06cb4ec
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-07 09:17:04 -0700

    Create empty hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ * Return: Returns a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* table;
+	unsigned long int i;
+
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
