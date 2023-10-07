175ddb7b94804175458ac81996fa028b2090a2e1
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-07 09:27:36 -0700

    Create empty hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
+	table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t));
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
