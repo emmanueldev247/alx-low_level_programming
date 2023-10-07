1f0c3300807061643eba82a94f546d060ea33c40
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-07 09:21:33 -0700

    Create empty hash table (+betty fixes)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table;
+	hash_table_t *table;
 	unsigned long int i;
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
-	for (i = 0; i < size; i++)
+	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
 	return (table);
