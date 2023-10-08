0b5489a9a2802119392b1a4176b8fbabc8633262
Emmanuel Ademola <mailemmydee@gmail.com>
2023-10-08 09:36:45 -0700

    adding elements to hash table (mod 1)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new_node = create_node(key, value);
+	if (new_node == NULL)
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	current_node = ht->array[index];
@@ -32,13 +35,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	return (1);
 }
 
-hash_node_t *create_node(const char* key, const char* value)
+hash_node_t *create_node(const char *key, const char *value)
 {
 	hash_node_t *node;
 
 	node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+
 	node->key = (char *) malloc(strlen(key) + 1);
+	if (node->key == NULL)
+		return (NULL);
+
 	node->value = (char *) malloc(strlen(value) + 1);
+	if (node->value == NULL)
+		return (NULL);
+
 	strcpy(node->key, key);
 	strcpy(node->value, value);
 
