4b91dbe41a7d207858dba867fb8686ab39007ffb
emmanueldev247 <mailemmydee@gmail.com>
2023-12-21 08:00:02 +0100

    updated: progressing (backup)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,5 +2,39 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-    val = 1
-    return val
+    totalPer = 0
+
+    rowSize = len(grid)
+    colSize = len(grid[0])
+
+    print(rowSize) #5
+    print(colSize) #6
+
+    for x in range (rowSize):
+        for y in range(colSize):
+            perEach = 4
+            if grid[x][y] == 1:
+                try:
+                    if grid[x][y+1] == 1:
+                        perEach -= 1
+                except:
+                    pass
+                try:
+                    if grid[x][y-1] == 1:
+                        perEach -= 1
+                except:
+                    pass
+                try:
+                    if grid[x+1][y] == 1:
+                        perEach -= 1
+                except:
+                    pass
+                try:
+                    if grid[x-1][y+1] == 1:
+                        perEach -= 1
+                except:
+                    pass
+
+            totalPer += perEach
+
+    return totalPer
