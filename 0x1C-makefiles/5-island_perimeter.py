c5bd6919fb8a658bff0fdcea3fd3ee9f3788e056
emmanueldev247 <mailemmydee@gmail.com>
2023-12-22 09:43:51 +0100

    updated: progressing (backup)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,9 +14,12 @@ def island_perimeter(grid):
         for y in range(colSize):
             perEach = 4
             if grid[x][y] == 1:
+                print(f"cell: grid[{x}][{y}]")
                 try:
                     if grid[x][y+1] == 1:
                         perEach -= 1
+                    else:
+                        print("no")
                 except:
                     pass
                 try:
